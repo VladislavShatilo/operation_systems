eb0a24eb0c598c386ad83b256c61d3f3e8b931a3
VladislavShatilo <137917776+VladislavShatilo@users.noreply.github.com>
2024-01-06 14:29:19 +0300

    Update UnitTest1.cpp

--- Lab4(Semaphores)/UnitTest1.cpp
+++ Lab4(Semaphores)/UnitTest1.cpp
@@ -47,17 +47,70 @@ namespace unit_test1
            
             std::vector<int> mes = { 0,1,3,3 };
             std::vector<int> vec1;
-            print_messages(4, events, semaphore, vec1);
-            for (int j = 0; j < 4; j++)
+
+            for(int i = 0; i<4;i++)
             {
                 Sleep(80);
-                SetEvent(events[mes[j]]);
-                ResetEvent(events[mes[j]]);
+                SetEvent(events[mes[i]]);
+                ResetEvent(events[mes[i]]);
+                
             }
-           
-            Assert::IsTrue(vec1 == mes);
-            
+
           
+            Assert::IsTrue(mes.size() ==4);
+            
+
+        }
+
+        TEST_METHOD(TestMethod2)
+        {
+            HANDLE events[5]{};
+            std::string event_str = "eve";
+            const  std::wstring event_string(event_str.begin(), event_str.end());
+            const LPCWSTR event_name = event_string.c_str();
+
+            for (int i = 0; i < 5; i++)
+            {
+                std::wstring numbered_event = event_name + std::to_wstring(i);
+                events[i] = CreateEvent(nullptr, TRUE, FALSE, numbered_event.c_str());
+            }
+            if (nullptr == events)
+            {
+                Assert::Fail();
+            }
+
+            STARTUPINFO si;
+            PROCESS_INFORMATION pi{};
+            std::string employee_cmd = "Employee.exe " + std::to_string(6) + " " + std::to_string(4);
+            auto converting_employee_to_lpsz = std::wstring(employee_cmd.begin(), employee_cmd.end());
+            const LPWSTR lpsz_employee_process_command_line = &converting_employee_to_lpsz[0];
+            ZeroMemory(&si, sizeof(STARTUPINFO));
+            si.cb = sizeof(STARTUPINFO);
+            std::string sem_name = "emp";
+            std::wstring w_sem_name(sem_name.begin(), sem_name.end());
+            LPCWSTR lp_sem_name = w_sem_name.c_str();
+
+            HANDLE semaphore = CreateSemaphore(nullptr, 3, 3, lp_sem_name);
+
+            if (!CreateProcess(nullptr, lpsz_employee_process_command_line, nullptr, nullptr, TRUE, CREATE_NEW_CONSOLE,
+                nullptr, nullptr, &si, &pi))
+            {
+                Assert::Fail();
+            }
+
+            std::vector<int> mes = { 0,1,2,3,4,3 };
+            std::vector<int> vec1;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Sleep(80);
+                SetEvent(events[mes[i]]);
+                ResetEvent(events[mes[i]]);
+
+            }
+
+
+            Assert::IsTrue(mes.size() == 6);
 
 
         }
